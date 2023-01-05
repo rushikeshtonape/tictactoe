b5fed78fa182522c035868e014f0a46d80407bae
rushikeshtonape <rushikeshtonape143@gmail.com>
2023-01-05 19:31:32 +0530

    Creating Board and Sqaure Component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Board from "./components/Board";
+
+const app = () => (
+  <>
+    <div>
+      <h1>TICTACTOE</h1>
+      <Board />
+    </div>
+  </>
+);
+
+export default app;
